8cbf3a9ec12775ada31d82b82a6337e82f722edc
BenGeordie <school@bengeordie@gmail.com>
2022-12-10 03:51:19 -0600

    styled email.js

--- src/email.js
+++ src/email.js
@@ -1,11 +1,12 @@
 import emailjs from '@emailjs/browser';
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import { Heading, Button, Input, FormLabel, Container, Box, Flex, Spacer } from "@chakra-ui/react";
 
 // TODO: Make new WhatTheySAI email so it doesn't get sent from "daniel johnson"
 // TODO: CSS
-// TODO: Use Chakra form elements for display, hide html form.
 export const EmailPage = () => {
   const form = useRef();
+  const [userEmail, setUserEmail] = useState("");
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -21,7 +22,7 @@ export const EmailPage = () => {
   const getDate = () => {
     const date_obj = new Date();
     
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const day = days[date_obj.getDay()];
     
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -41,14 +42,36 @@ export const EmailPage = () => {
   const final_thoughts = "These are your final thoughts"; // TODO Get from state
   
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <input type="text" name="date" value={getDate()} hidden={true} />
-      <textarea name="transcript" value={transcript} hidden={true} />
-      <textarea name="work_on_notes" value={to_work_on} hidden={true} />
-      <textarea name="final_thoughts" value={final_thoughts} hidden={true} />
-      <input type='submit' value='Send' />
-    </form>
+    
+    <Flex h='50vh' flexDirection={'column'}>
+      <Box bg='none' h='10vmin'/>
+      <Container>
+        <Heading>Send Email</Heading>
+      </Container>
+
+      <Spacer/>
+
+      <Container>
+        <FormLabel>Email address</FormLabel>
+        <Input 
+          focusBorderColor='black'
+          placeholder='Your email' 
+          onChange={(event) => setUserEmail(event.target.value)}
+        />
+        <Box bg='none' h='20px'/>
+        <Button bg='black' color='white' onClick={sendEmail}>Send</Button>
+      </Container>
+
+      {/* Hidden HTML Form Element to pass into emailjs API */}
+      <form ref={form} hidden={true}>
+        <label>Email</label>
+        <input type="email" name="user_email" value={userEmail}/>
+        <input type="text" name="date" value={getDate()} hidden={true} />
+        <textarea name="transcript" value={transcript} hidden={true} />
+        <textarea name="work_on_notes" value={to_work_on} hidden={true} />
+        <textarea name="final_thoughts" value={final_thoughts} hidden={true} />
+      </form>
+    </Flex>
+    
   );
 };
